37f2c956e85935dc491534a83423d8c53ecb1f95
rajvardhan97 <n.rajvardhan2627@gmail.com>
2022-05-11 16:13:45 +0530

    [Raj]Add: Find Nth Harmonic Number

---
+++ HarmonicNumber.cs
@@ -0,0 +1,24 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace BasicProgram
+{
+    internal class HarmonicNumber
+    {
+        public void Harmonic()
+        {
+            float value = 0;
+            Console.WriteLine("Enter a number to find Nth harmonic number ");
+            int n = Convert.ToInt32(Console.ReadLine());
+            for(float i = 1; i <= n; i++)
+            {
+              value += 1 / i;
+            }
+
+            Console.WriteLine("Nth Harmonic number is " + value);
+        }
+    }
+}
