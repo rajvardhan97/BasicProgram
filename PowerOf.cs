de97e2feeeffa68e6a26ae623894e66532632b6d
rajvardhan97 <n.rajvardhan2627@gmail.com>
2022-05-11 15:47:30 +0530

    [Raj]Add: Find power of 2 upto nth term

---
+++ PowerOf.cs
@@ -0,0 +1,27 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace BasicProgram
+{
+    internal class PowerOf
+    {
+        public void power()
+        {
+            Console.WriteLine("Enter a number");
+            int Num = Convert.ToInt32(Console.ReadLine());
+
+            while(Num < 0 || Num >= 31)
+            {
+                Console.WriteLine("Enter a Valid Number");
+                Num = Convert.ToInt32(Console.ReadLine());
+            }
+            for(int i = 0; i <= Num; i++)
+            {
+                Console.WriteLine("2^" + i , (Math.Pow(2, i)));
+            }
+        }
+    }
+}
