3216e3cf4e2cc7e44d3e46d394c49ea8c585eacc
rajvardhan97 <n.rajvardhan2627@gmail.com>
2022-05-12 08:20:33 +0530

    [Raj]Add: Find Quotient and Remainder

---
+++ QuotientRemainder.cs
@@ -0,0 +1,29 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace BasicProgram
+{
+    internal class QuotientRemainder
+    {
+        public void qnr()
+        {
+            int quotient = 0, remainder = 0;
+
+            Console.Write("Enter Dividend first : ");
+            int dividend = Convert.ToInt32(Console.ReadLine());
+            Console.Write("Enter Divisor : ");
+            int divisor = Convert.ToInt32(Console.ReadLine());
+
+            quotient = dividend / divisor;
+
+            remainder = dividend % divisor;
+
+            Console.WriteLine("Quotient is: " + quotient);
+            Console.WriteLine("Remainder is: " + remainder);
+
+        }
+    }
+}
