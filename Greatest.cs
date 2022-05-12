a9abfce6a699f2d8e67f91956ab994599bd873fb
rajvardhan97 <n.rajvardhan2627@gmail.com>
2022-05-12 10:25:47 +0530

    [Raj]Add: Find greatest of 3 numbers

---
+++ Greatest.cs
@@ -0,0 +1,36 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace BasicProgram
+{
+    internal class Greatest
+    {
+        public void largest()
+        {
+            Console.WriteLine("Enter the first number ");
+            int first = Convert.ToInt32(Console.ReadLine());
+            Console.WriteLine("Enter the second number ");
+            int second = Convert.ToInt32(Console.ReadLine());
+            Console.WriteLine("Enter the third number ");
+            int third = Convert.ToInt32(Console.ReadLine());
+            int max = first;
+
+            if (first >= second && first >= third)
+            {
+                max = first;
+            }
+            else if (second >= first && second >= third)
+            {
+                max = second;
+            }
+            else if (third >= first && third >= second)
+            {
+                max = third;
+            }
+            Console.WriteLine("Greatest number is " + max);
+        }
+    }
+}
